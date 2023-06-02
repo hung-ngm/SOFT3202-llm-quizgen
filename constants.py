0fd4742f8a63891a866a447636430f8039875017
minhhung123 <prohungnguyenminh@gmail.com>
2023-06-03 00:40:05 +1000

    Fix small bugs

--- constants.py
+++ constants.py
@@ -1,11 +1,7 @@
-import os
-from dotenv import load_dotenv
 from chromadb.config import Settings
 
-load_dotenv()
-
 # Define the folder for storing database
-PERSIST_DIRECTORY = os.environ.get("PERSIST_DIRECTORY")
+PERSIST_DIRECTORY = "db"
 
 # Define the Chroma settings
 CHROMA_SETTINGS = Settings(
